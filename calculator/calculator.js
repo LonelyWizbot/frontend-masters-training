08351cc42dca24349828fda95e77a1b374e24388
Paul Cabot <paul.claude.cabot@gmail.com>
2025-06-02 17:45:35 +0200

    Adding modifications to the event listener so that only numbers can appear in the result box

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,5 +2,7 @@ const button = document.querySelector(".grid");
 const result = document.querySelector(".result");
 
 button.addEventListener("click", function(event) {
+  if (event.target.classList.contains("number")) {
   result.textContent = event.target.textContent;
+  }
 });
