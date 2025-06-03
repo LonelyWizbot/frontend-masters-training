65fdeaad222777b6a1c051bec8d71d41348a6184
Paul Cabot <paul.claude.cabot@gmail.com>
2025-06-03 09:48:03 +0200

    Corrected the condition for the event listener

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,13 +2,21 @@ const button = document.querySelector(".grid");
 const result = document.querySelector(".result");
 
 button.addEventListener("click", function(event) {
-  if (event.target.classList.contains("number")) {
-  result.textContent = parseInt(event.target.textContent);
+  if (event.target.classList.contains("number") && result.textContent === "0") {
+  result.textContent = event.target.textContent;
+  } else if (event.target.classList.contains("number")) {
+    result.textContent += event.target.textContent;
   } else if (event.target.classList.contains("c")) {
     clear();
+  } else if (event.target.classList.contains("arrow")) {
+    arrow();
   }
 });
 
 function clear() {
-  result.textContent = parseInt(0);
+  result.textContent = 0;
+}
+
+function arrow() {
+  result.textContent = result.textContent.pop();
 }
