7e2389c6347427d67264367d67c7d144d6989d09
Paul Cabot <paul.claude.cabot@gmail.com>
2025-06-03 08:56:06 +0200

    Adding logic for clear function

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,5 +4,11 @@ const result = document.querySelector(".result");
 button.addEventListener("click", function(event) {
   if (event.target.classList.contains("number")) {
   result.textContent = parseInt(event.target.textContent);
+  } else if (event.target.classList.contains("c")) {
+    clear();
   }
 });
+
+function clear() {
+  result.textContent = parseInt(0);
+}
