d321c1e006a5c0ea316d4eed6ace8683984d37d8
Paul Cabot <paul.claude.cabot@gmail.com>
2025-06-04 18:21:33 +0200

    First draft of event listener for isLetter

--- word-masters/word.js
+++ word-masters/word.js
@@ -1,5 +1,13 @@
-let answer = document.querySelector(".input-box");
+let answer = document.querySelector(".tester-input");
 
 function isLetter(letter) {
   return /^[a-zA-Z]$/.test(letter);
 }
+
+answer.addEventListener("keydown", function (event) {
+  if (!isLetter(event.key)) {
+    event.preventDefault();
+  } else {
+    console.log("this is a letter")
+  };
+});
